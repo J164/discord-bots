13ef0701867783424ac345a0ff3a9adf069a0161
J164 <43323130+J164@users.noreply.github.com>
2021-11-08 20:25:08 -0600

    improve deck storer

--- src/bots/krenko/commands/addDeck.ts
+++ src/bots/krenko/commands/addDeck.ts
@@ -1,5 +1,5 @@
+import axios from 'axios'
 import { ApplicationCommandData, CommandInteraction, InteractionReplyOptions } from 'discord.js'
-import { Deck } from '../../../core/modules/Deck'
 import { GuildInfo } from '../../../core/utils/interfaces'
 
 const data: ApplicationCommandData = {
@@ -14,21 +14,25 @@ const data: ApplicationCommandData = {
 }
 
 async function addDeck(interaction: CommandInteraction, info: GuildInfo): Promise<InteractionReplyOptions> {
-    const deck = new Deck()
-    if (!await deck.getInfo(<string> interaction.options.getString('url'))) {
-        return { content: 'Something went wrong... (Make sure you are using a valid deck url from deckstats.net)' }
+    let apiUrl: string
+    let name: string
+    try {
+        const fields = interaction.options.getString('url').split('/')
+        const authorID = fields[4]
+        const deckID = fields[5].split('-')[0]
+        apiUrl = `https://deckstats.net/api.php?action=get_deck&id_type=saved&owner_id=${authorID}&id=${deckID}&response_type=`
+        name = (await axios.get(`${apiUrl}json`)).data.name
+    } catch {
+        return { content: 'Something went wrong (Make sure you are using a deckstats url' }
     }
     try {
-        info.database.insert('decks', new Map<string, string>([
-            [ 'name', deck.getName() ],
-            [ 'image', deck.getImage() ],
-            [ 'url', deck.getUrl() ],
-            [ 'api_url', deck.getApiUrl() ]
+        info.database.insert('mtg_decks', new Map<string, string>([
+            [ 'url', interaction.options.getString('url') ]
         ]))
     } catch {
         return { content: 'Failed! (Make sure the deck isn\'t a duplicate)' }
     }
-    return { content: 'Success!' }
+    return { content: `Success! Deck "${name}" has been added!` }
 }
 
 module.exports = { data: data, execute: addDeck }
--- src/bots/krenko/commands/decks.ts
+++ src/bots/krenko/commands/decks.ts
@@ -1,22 +1,49 @@
 import { ApplicationCommandData, ButtonInteraction, CollectorFilter, CommandInteraction, InteractionCollector, InteractionReplyOptions, MessageActionRow, MessageButton } from 'discord.js'
-import { GuildInfo } from '../../../core/utils/interfaces'
-import { Deck } from '../../../core/modules/Deck'
+import { GuildInfo, ScryfallResponse } from '../../../core/utils/interfaces'
+import { genericEmbed } from '../../../core/utils/commonFunctions'
+import axios from 'axios'
 
 const data: ApplicationCommandData = {
     name: 'decks',
     description: 'Get a deck from Krenko\'s database'
 }
 
-interface DeckInfo {
-    readonly name: string,
-    readonly image: string,
-    readonly url: string,
-    readonly api_url: string
+interface DeckstatsResponse {
+    readonly name: string
+    readonly sections: readonly {
+        readonly cards: readonly {
+            readonly name: string
+            readonly isCommander: boolean
+        }[]
+    }[]
 }
 
-async function parseDeck(interaction: CommandInteraction, info: GuildInfo, decks: DeckInfo[], button: ButtonInteraction = null, i = 0): Promise<void> {
-    const deck = new Deck()
-    deck.fill(decks[i])
+async function getList(url: string): Promise<string> {
+    const decklist = <string> (await axios.get(`${url}list`)).data.list
+    const decklistArray = decklist.split('\n')
+    for (let i = 0; i < decklistArray.length; i++) {
+        if (!decklistArray[i] || decklistArray[i].startsWith('//')) {
+            decklistArray.splice(i, 1)
+            i--
+            continue
+        }
+        if (decklistArray[i].indexOf('//') !== -1) {
+            decklistArray[i] = decklistArray[i].substr(0, decklistArray[i].indexOf('//'))
+        }
+        if (decklistArray[i].indexOf('#') !== -1) {
+            decklistArray[i] = decklistArray[i].substr(0, decklistArray[i].indexOf('#'))
+        }
+    }
+    return '\n' + decklistArray.join('\n')
+}
+
+async function parseDeck(interaction: CommandInteraction, info: GuildInfo, urls: { url: string }[], button: ButtonInteraction = null, i = 0): Promise<void> {
+    const url = urls[i].url
+    const fields = url.split('/')
+    const authorID = fields[4]
+    const deckID = fields[5].split('-')[0]
+    const apiUrl = `https://deckstats.net/api.php?action=get_deck&id_type=saved&owner_id=${authorID}&id=${deckID}&response_type=`
+    const results = <DeckstatsResponse> (await axios.get(`${apiUrl}json`)).data
     const components = [ new MessageButton({ customId: 'decks-doublearrowleft', emoji: '\u23EA', label: 'Return to Beginning', style: 'SECONDARY' }),
                          new MessageButton({ customId: 'decks-arrowleft', emoji: '\u2B05\uFE0F', label: 'Previous Page', style: 'SECONDARY' }),
                          new MessageButton({ customId: 'decks-list', emoji: '\uD83D\uDCC4', label: 'Decklist', style: 'PRIMARY' }),
@@ -26,12 +53,20 @@ async function parseDeck(interaction: CommandInteraction, info: GuildInfo, decks
         components[0].setDisabled(true)
         components[1].setDisabled(true)
     }
-    if (i === decks.length - 1) {
+    if (i === urls.length - 1) {
         components[3].setDisabled(true)
         components[4].setDisabled(true)
     }
     const row1 = new MessageActionRow().addComponents(components)
-    const options: InteractionReplyOptions = { embeds: [ deck.getPreview().setFooter(`${i + 1}/${decks.length}`) ], components: [ row1 ] }
+    let image: string
+    for (const section of results.sections) {
+        const commander = section.cards.findIndex(card => card.isCommander)
+        if (commander !== -1) {
+            const cardInfo = <ScryfallResponse> (await axios.get(`https://api.scryfall.com/cards/search?q=${encodeURIComponent(section.cards[commander].name)}`)).data
+            image = cardInfo.data[0].image_uris.large
+        }
+    }
+    const options: InteractionReplyOptions = { embeds: [ genericEmbed({ title: results.name, image: { url: image }, fields: [ { name: 'Deckstats URL:', value: url } ], footer: { text: `${i + 1}/${urls.length}` } }) ], components: [ row1 ] }
     if (!button) {
         await interaction.editReply(options)
     } else {
@@ -43,23 +78,23 @@ async function parseDeck(interaction: CommandInteraction, info: GuildInfo, decks
     collector.once('collect', async b => {
         switch (b.customId) {
             case 'decks-doublearrowleft':
-                parseDeck(interaction, info, decks, b)
+                parseDeck(interaction, info, urls, b)
                 break
             case 'decks-arrowleft':
-                parseDeck(interaction, info, decks, b, i - 1)
+                parseDeck(interaction, info, urls, b, i - 1)
                 break
             case 'decks-list':
                 try {
-                    b.update({ content: await deck.getList(), embeds: [], components: [] })
+                    b.update({ content: await getList(apiUrl), embeds: [], components: [] })
                 } catch {
                     b.update({ content: 'There seems to be something wrong with the Deckstats API at the moment. Try again later', embeds: [], components: [] })
                 }
                 break
             case 'decks-arrowright':
-                parseDeck(interaction, info, decks, b, i + 1)
+                parseDeck(interaction, info, urls, b, i + 1)
                 break
             case 'decks-doublearrowright':
-                parseDeck(interaction, info, decks, b, decks.length - 1)
+                parseDeck(interaction, info, urls, b, urls.length - 1)
                 break
             default:
                 break
@@ -69,8 +104,8 @@ async function parseDeck(interaction: CommandInteraction, info: GuildInfo, decks
 }
 
 function getDeck(interaction: CommandInteraction, info: GuildInfo): void {
-    info.database.select('decks', results => {
-        parseDeck(interaction, info, <DeckInfo[]> results)
+    info.database.select('mtg_decks', results => {
+        parseDeck(interaction, info, <{ url: string }[]> results)
     })
 }
 
