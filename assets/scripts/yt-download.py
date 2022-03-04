f61e11651c7d5f099e66cea6a7dd83b9baf7810d
J164 <43323130+J164@users.noreply.github.com>
2022-03-04 13:30:47 -0600

    improve ytdl again

--- assets/scripts/yt-download.py
+++ assets/scripts/yt-download.py
@@ -3,7 +3,4 @@ import sys
 import yt_dlp
 
 with yt_dlp.YoutubeDL(json.loads(sys.argv[2])) as ytdl:
-    try:
-        ytdl.download([ sys.argv[1] ])
-    except:
-        print(json.dumps({ "success": False }))
+    ytdl.download([ sys.argv[1] ])
--- src/core/modules/ytdl.ts
+++ src/core/modules/ytdl.ts
@@ -1,4 +1,4 @@
-import { ChildProcessByStdio, spawn } from 'node:child_process'
+import { ChildProcessByStdio, exec, spawn } from 'node:child_process'
 import { Readable } from 'node:stream'
 
 export function createStream(url: string, options: unknown): ChildProcessByStdio<null, Readable, null> {
@@ -7,25 +7,19 @@ export function createStream(url: string, options: unknown): ChildProcessByStdio
 
 export async function resolve(url: string, options: unknown): Promise<unknown> {
     return new Promise(resolve => {
-        const script = spawn('python3', [ '-u', `./assets/scripts/yt-resolve.py`, url, JSON.stringify(options) ], { stdio: [ 'ignore', 'pipe', 'ignore' ] })
-
-        script.stdout.once('data', (data: unknown) => {
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-            resolve(JSON.parse(data.toString()))
+        exec(`python3 -u ./assets/scripts/yt-resolve.py ${url} ${JSON.stringify(options).replaceAll('"', '\\"')}`, (error, stdout) => {
+            resolve(JSON.parse(stdout))
         })
     })
 }
 
 export async function download(url: string, options: unknown): Promise<void> {
     return new Promise((resolve, reject) => {
-        const script = spawn('python3', [ '-u', `./assets/scripts/yt-download.py`, url, JSON.stringify(options) ], { stdio: [ 'ignore', 'pipe', 'ignore' ] })
-
-        script.stdout.once('data', () => {
-            script.removeAllListeners('exit')
-            reject()
-        })
-        script.once('exit', () => {
-            resolve()
+        exec(`python3 -u ./assets/scripts/yt-download.py ${url} ${JSON.stringify(options).replaceAll('"', '\\"')}`, (error) => {
+            if (error) {
+                return reject()
+            }
+            return resolve()
         })
     })
 }
